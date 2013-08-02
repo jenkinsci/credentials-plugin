8c9bab04cc01657351ca84c894f32a6b680a6bd6
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-08-02 16:07:59 +0100

    We need a standard contract for equals and hashCode and what to do with empty ids

--- src/main/java/com/cloudbees/plugins/credentials/common/IdCredentials.java
+++ src/main/java/com/cloudbees/plugins/credentials/common/IdCredentials.java
@@ -24,7 +24,11 @@
 package com.cloudbees.plugins.credentials.common;
 
 import com.cloudbees.plugins.credentials.Credentials;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.UUID;
 
 /**
  * Credentials that have an unique ID that assists in retrieving the specific credential from a collection of
@@ -40,4 +44,57 @@ public interface IdCredentials extends Credentials {
      */
     @NonNull
     String getId();
+
+    /**
+     * The contract implementations of {@link #equals(Object)} and {@link #hashCode()} that implementations of
+     * this class must use as the basis for equality tests and as the hash code.
+     *
+     * @since 1.6
+     */
+    public static final class Helpers {
+        /**
+         * Standard {@link #equals(Object)} implementation.
+         *
+         * @param self the {@code this} reference.
+         * @param o    the other object.
+         * @return true if equal.
+         */
+        public static boolean equals(IdCredentials self, Object o) {
+            if (self == o) {
+                return true;
+            }
+            if (!(o instanceof IdCredentials)) {
+                return false;
+            }
+
+            IdCredentials that = (IdCredentials) o;
+
+            if (!self.getId().equals(that.getId())) {
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * The standard {@link #hashCode()} implementation.
+         *
+         * @param self the {@code this} reference.
+         * @return the hash code.
+         */
+        public static int hashCode(IdCredentials self) {
+            return self.getId().hashCode();
+        }
+
+        /**
+         * Returns either the id or a generated new id if the supplied id is missing.
+         *
+         * @param id the supplied id.
+         * @return either the id or a generated new id if the supplied id is missing.
+         */
+        @NonNull
+        public static String fixEmptyId(@CheckForNull String id) {
+            return StringUtils.isEmpty(id) ? UUID.randomUUID().toString() : id;
+        }
+    }
 }
