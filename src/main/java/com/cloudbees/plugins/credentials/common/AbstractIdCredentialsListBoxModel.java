42b2bca19d5b18469281a98f0341bd43011d9364
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-08-07 11:50:08 +0100

    Helper ListBoxModel implementations

---
+++ src/main/java/com/cloudbees/plugins/credentials/common/AbstractIdCredentialsListBoxModel.java
@@ -0,0 +1,157 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011-2013, CloudBees, Inc., Stephen Connolly.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cloudbees.plugins.credentials.common;
+
+import com.cloudbees.plugins.credentials.CredentialsMatcher;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.util.ListBoxModel;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Base class for {@link ListBoxModel} types that present a selection of credentials where the selection is keyed
+ * by the credential's {@link com.cloudbees.plugins.credentials.common.IdCredentials#getId()}.
+ *
+ * @since 1.6
+ */
+public abstract class AbstractIdCredentialsListBoxModel<T extends AbstractIdCredentialsListBoxModel<T, C>,
+        C extends IdCredentials>
+        extends ListBoxModel {
+
+    /**
+     * Generate a description of the supplied credential.
+     *
+     * @param c the credential.
+     * @return the description.
+     */
+    @NonNull
+    protected abstract String describe(@NonNull C c);
+
+    /**
+     * Adds a single credential.
+     *
+     * @param u the credential to add.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> add(@CheckForNull C u) {
+        if (u != null) {
+            add(describe(u), u.getId());
+        }
+        return this;
+    }
+
+    /**
+     * Adds an "empty" credential to signify selection of no credential.
+     *
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addEmptySelection() {
+        add(Messages.AbstractIdCredentialsListBoxModel_EmptySelection(), "");
+        return this;
+    }
+
+    /**
+     * Adds supplied credentials to the model.
+     *
+     * @param credentials the credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addAll(@NonNull C... credentials) {
+        return addMatching(CredentialsMatchers.always(), Arrays.asList(credentials));
+    }
+
+    /**
+     * Adds supplied credentials to the model.
+     *
+     * @param credentials the credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addAll(@NonNull Iterable<? extends C> credentials) {
+        return addMatching(CredentialsMatchers.always(), credentials.iterator());
+    }
+
+    /**
+     * Adds supplied credentials to the model.
+     *
+     * @param credentials the credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addAll(@NonNull Iterator<? extends C> credentials) {
+        return addMatching(CredentialsMatchers.always(), credentials);
+    }
+
+    /**
+     * Adds the matching subset of suppled credentials to the model.
+     *
+     * @param matcher     the matcher.
+     * @param credentials the superset of credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addMatching(@NonNull CredentialsMatcher matcher,
+                                                               @NonNull C... credentials) {
+        return addMatching(matcher, Arrays.asList(credentials));
+    }
+
+    /**
+     * Adds the matching subset of suppled credentials to the model.
+     *
+     * @param matcher     the matcher.
+     * @param credentials the superset of credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addMatching(@NonNull CredentialsMatcher matcher,
+                                                               @NonNull Iterable<? extends C> credentials) {
+        return addMatching(matcher, credentials.iterator());
+    }
+
+    /**
+     * Adds the matching subset of suppled credentials to the model.
+     *
+     * @param matcher     the matcher.
+     * @param credentials the superset of credentials.
+     * @return {@code this} for method chaining.
+     */
+    @NonNull
+    public AbstractIdCredentialsListBoxModel<T, C> addMatching(@NonNull CredentialsMatcher matcher,
+                                                               @NonNull Iterator<? extends C> credentials) {
+        while (credentials.hasNext()) {
+            C c = credentials.next();
+            if (matcher.matches(c)) {
+                add(c);
+            }
+        }
+        return this;
+    }
+
+}
---
+++ src/main/java/com/cloudbees/plugins/credentials/common/StandardUsernameListBoxModel.java
@@ -0,0 +1,47 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011-2013, CloudBees, Inc., Stephen Connolly.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cloudbees.plugins.credentials.common;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Util;
+import hudson.util.ListBoxModel;
+
+/**
+ * {@link ListBoxModel} with {@link StandardUsernameCredentials} support.
+ *
+ * @since 1.6
+ */
+public class StandardUsernameListBoxModel
+        extends AbstractIdCredentialsListBoxModel<StandardUsernameListBoxModel, StandardUsernameCredentials> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @NonNull
+    protected String describe(@NonNull StandardUsernameCredentials c) {
+        String description = Util.fixEmptyAndTrim(c.getDescription());
+        return c.getUsername() + (description != null ? " (" + description + ")" : "");
+    }
+
+}
