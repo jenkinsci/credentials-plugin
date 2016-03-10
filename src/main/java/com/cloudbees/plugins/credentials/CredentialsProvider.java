93e7492d6ab9d9342db577afa6a47dd70de8d982
Andrae Raymond <draeisme@gmail.com>
2016-03-10 08:19:54 -0600

    [JENKINS-31863] - minor fix changed tabs to spaces

--- src/main/java/com/cloudbees/plugins/credentials/CredentialsProvider.java
+++ src/main/java/com/cloudbees/plugins/credentials/CredentialsProvider.java
@@ -844,8 +844,8 @@ public abstract class CredentialsProvider implements ExtensionPoint {
  * Comparator to alphabetically sort credentials drop down list in ascending order by Credential Name.
  *
  */
-class CredentialsNameComparator implements Comparator<Credentials>, java.io.Serializable{
-	private static final long serialVersionUID = 1;
+class CredentialsNameComparator implements Comparator<Credentials>, java.io.Serializable {
+    private static final long serialVersionUID = 1;
     @Override
     public int compare(Credentials cred1, Credentials cred2) {
         return CredentialsNameProvider.name(cred1).compareTo(CredentialsNameProvider.name(cred2));
