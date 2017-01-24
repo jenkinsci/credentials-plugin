1e606c990c2b5bb6c543fe0b001d6e747cbeaf2d
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-01-24 11:54:58 +0000

    Add some tests with a queue item authenticator

--- src/test/java/com/cloudbees/plugins/credentials/SystemCredentialsProviderTest.java
+++ src/test/java/com/cloudbees/plugins/credentials/SystemCredentialsProviderTest.java
@@ -23,11 +23,32 @@
  */
 package com.cloudbees.plugins.credentials;
 
+import com.cloudbees.plugins.credentials.common.IdCredentials;
 import com.cloudbees.plugins.credentials.impl.DummyCredentials;
+import com.cloudbees.plugins.credentials.impl.DummyIdCredentials;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.Computer;
+import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.model.Result;
+import hudson.model.User;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import java.io.IOException;
+import java.util.HashMap;
+import jenkins.security.QueueItemAuthenticatorConfiguration;
+import org.acegisecurity.Authentication;
 import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.MockAuthorizationStrategy;
+import org.jvnet.hudson.test.MockQueueItemAuthenticator;
+import org.jvnet.hudson.test.TestExtension;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -70,6 +91,119 @@ public class SystemCredentialsProviderTest {
         assertEquals(true, !CredentialsProvider.allCredentialsDescriptors().isEmpty());
         assertNotNull(SystemCredentialsProvider.getInstance().getDescriptor());
         assertNotNull(SystemCredentialsProvider.getInstance().getCredentials());
+    }
+
+    @Test
+    public void given_globalScopeCredential_when_builtAsSystem_then_credentialFound() throws Exception {
+        SystemCredentialsProvider.getInstance().getCredentials().add(
+                new DummyIdCredentials("foo-manchu", CredentialsScope.GLOBAL, "foo", "manchu", "Dr. Fu Manchu")
+        );
+        FreeStyleProject prj = r.createFreeStyleProject();
+        prj.getBuildersList().add(new HasCredentialBuilder("foo-manchu"));
+        r.buildAndAssertSuccess(prj);
+    }
+
+    @Test
+    public void given_systemScopeCredential_when_builtAsSystem_then_credentialNotFound() throws Exception {
+        SystemCredentialsProvider.getInstance().getCredentials().add(
+                new DummyIdCredentials("foo-manchu", CredentialsScope.SYSTEM, "foo", "manchu", "Dr. Fu Manchu")
+        );
+        FreeStyleProject prj = r.createFreeStyleProject();
+        prj.getBuildersList().add(new HasCredentialBuilder("foo-manchu"));
+        r.assertBuildStatus(Result.FAILURE, prj.scheduleBuild2(0).get());
+    }
+
+    @Test
+    public void given_globalScopeCredential_when_builtAsUserWithUseItem_then_credentialFound() throws Exception {
+        SystemCredentialsProvider.getInstance().getCredentials().add(
+                new DummyIdCredentials("foo-manchu", CredentialsScope.GLOBAL, "foo", "manchu", "Dr. Fu Manchu")
+        );
+        FreeStyleProject prj = r.createFreeStyleProject();
+        prj.getBuildersList().add(new HasCredentialBuilder("foo-manchu"));
+
+        JenkinsRule.DummySecurityRealm realm = r.createDummySecurityRealm();
+        r.jenkins.setSecurityRealm(realm);
+
+        MockAuthorizationStrategy strategy = new MockAuthorizationStrategy();
+        strategy.grant(CredentialsProvider.USE_ITEM).everywhere().to("bob");
+        strategy.grant(Item.BUILD).everywhere().to("bob");
+        strategy.grant(Computer.BUILD).everywhere().to("bob");
+
+        r.jenkins.setAuthorizationStrategy(strategy);
+        HashMap<String, Authentication> jobsToUsers = new HashMap<String, Authentication>();
+        jobsToUsers.put(prj.getFullName(), User.get("bob").impersonate());
+        MockQueueItemAuthenticator authenticator = new MockQueueItemAuthenticator(jobsToUsers);
+
+        QueueItemAuthenticatorConfiguration.get().getAuthenticators().clear();
+        QueueItemAuthenticatorConfiguration.get().getAuthenticators().add(authenticator);
+        r.buildAndAssertSuccess(prj);
+    }
+
+    @Test
+    public void given_globalScopeCredential_when_builtAsUserWithoutUseItem_then_credentialNotFound() throws Exception {
+        SystemCredentialsProvider.getInstance().getCredentials().add(
+                new DummyIdCredentials("foo-manchu", CredentialsScope.GLOBAL, "foo", "manchu", "Dr. Fu Manchu")
+        );
+        FreeStyleProject prj = r.createFreeStyleProject();
+        prj.getBuildersList().add(new HasCredentialBuilder("foo-manchu"));
+
+        JenkinsRule.DummySecurityRealm realm = r.createDummySecurityRealm();
+        r.jenkins.setSecurityRealm(realm);
+
+        MockAuthorizationStrategy strategy = new MockAuthorizationStrategy();
+        strategy.grant(Item.BUILD).everywhere().to("bob");
+        strategy.grant(Computer.BUILD).everywhere().to("bob");
+
+        r.jenkins.setAuthorizationStrategy(strategy);
+        HashMap<String, Authentication> jobsToUsers = new HashMap<String, Authentication>();
+        jobsToUsers.put(prj.getFullName(), User.get("bob").impersonate());
+        MockQueueItemAuthenticator authenticator = new MockQueueItemAuthenticator(jobsToUsers);
+
+        QueueItemAuthenticatorConfiguration.get().getAuthenticators().clear();
+        QueueItemAuthenticatorConfiguration.get().getAuthenticators().add(authenticator);
+        r.assertBuildStatus(Result.FAILURE, prj.scheduleBuild2(0).get());
+    }
+
+    public static class HasCredentialBuilder extends Builder {
+
+        private final String id;
+
+        @DataBoundConstructor
+        public HasCredentialBuilder(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+                throws InterruptedException, IOException {
+            IdCredentials credentials = CredentialsProvider.findCredentialById(id, IdCredentials.class, build);
+            if (credentials == null) {
+                listener.getLogger().printf("Could not find any credentials with id %s%n", id);
+                build.setResult(Result.FAILURE);
+                return false;
+            } else {
+                listener.getLogger().printf("Found %s credentials with id %s%n", CredentialsNameProvider.name(credentials), id);
+                return true;
+            }
+        }
+
+        @TestExtension
+        public static class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+            @Override
+            public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+                return true;
+            }
+
+            @Override
+            public String getDisplayName() {
+                return "Probe credentials exist";
+            }
+        }
     }
 
 }
---
+++ src/test/java/com/cloudbees/plugins/credentials/impl/DummyIdCredentials.java
@@ -0,0 +1,71 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011-2012, CloudBees, Inc., Stephen Connolly.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cloudbees.plugins.credentials.impl;
+
+import com.cloudbees.plugins.credentials.CredentialsDescriptor;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.util.Secret;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * A test credentials impl.
+ */
+public class DummyIdCredentials extends BaseStandardCredentials implements UsernamePasswordCredentials {
+
+    private final String username;
+
+    private final Secret password;
+
+    @DataBoundConstructor
+    public DummyIdCredentials(String id, CredentialsScope scope, String username, String password, String description) {
+        super(scope, id, description);
+        this.username = username;
+        this.password = Secret.fromString(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @NonNull
+    public Secret getPassword() {
+        return password;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends CredentialsDescriptor {
+
+        public DescriptorImpl() {
+            super();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Dummy Id Credentials";
+        }
+    }
+}
