626a8e8561e37c6e8978c5a0519d86940d6e90e9
amuniz <amunizmartin@gmail.com>
2020-04-14 10:02:13 +0200

    [JENKINS-61880] Test coverage

---
+++ src/test/java/com/cloudbees/plugins/credentials/casc/CredentialsCategoryTest.java
@@ -0,0 +1,57 @@
+package com.cloudbees.plugins.credentials.casc;
+
+import com.cloudbees.plugins.credentials.GlobalCredentialsConfiguration;
+import hudson.Extension;
+import hudson.ExtensionList;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import jenkins.model.GlobalConfiguration;
+import jenkins.model.GlobalConfigurationCategory;
+import org.jenkinsci.Symbol;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.TestExtension;
+
+import javax.annotation.Nonnull;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CredentialsCategoryTest {
+
+    @Rule
+    public JenkinsConfiguredWithCodeRule r = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    @ConfiguredWithCode("credentials-category.yaml")
+    public void globalCredentialsConfigurationCategory() {
+        assertThat(ExtensionList.lookupSingleton(TestGlobalConfiguration.class).getConfig(), equalTo("hello"));
+    }
+
+    @TestExtension
+    @Symbol("testGlobalConfiguration")
+    public static class TestGlobalConfiguration extends GlobalConfiguration {
+
+        private String config;
+
+        @Nonnull
+        @Override
+        public GlobalConfigurationCategory getCategory() {
+            return GlobalConfigurationCategory.get(GlobalCredentialsConfiguration.Category.class);
+        }
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return "Test Global Configuration";
+        }
+
+        public String getConfig() {
+            return config;
+        }
+
+        public void setConfig(String config) {
+            this.config = config;
+        }
+    }
+}
