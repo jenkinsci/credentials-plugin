5fb392f2485833a4d3ae44b69d948182c6706b90
Tim Jacomb <timjacomb1+github@gmail.com>
2020-05-25 12:05:05 +0100

    Add 'Manage Credentials' link to Manage Jenkins

---
+++ src/main/java/com/cloudbees/plugins/credentials/ManageCredentialsConfiguration.java
@@ -0,0 +1,43 @@
+package com.cloudbees.plugins.credentials;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.ManagementLink;
+import hudson.security.Permission;
+
+/**
+ * {@link ManagementLink} to expose a link to the manage credentials configuration screen.
+ *
+ */
+@Extension(ordinal = Integer.MAX_VALUE - 211)
+public class ManageCredentialsConfiguration extends ManagementLink {
+
+    public String getCategoryName() {
+        return "SECURITY";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "/plugin/credentials/images/48x48/credentials.png";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "credentials";
+    }
+
+    @Override
+    public String getDescription() {
+        return Messages.ManageCredentialsConfiguration_description();
+    }
+
+    public String getDisplayName() {
+        return Messages.ManageCredentialsConfiguration_displayName();
+    }
+
+    @NonNull
+    @Override
+    public Permission getRequiredPermission() {
+        return CredentialsProvider.VIEW;
+    }
+}
