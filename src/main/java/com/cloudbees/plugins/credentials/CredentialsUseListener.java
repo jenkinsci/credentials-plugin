342e74259c71c5aff06896cc18e6a0a07818e3b6
Meiswinkel, Jan  SF/HZA-ZC2S <meiswjn@schaeffler.com>
2022-03-31 09:58:39 +0200

    Make listener more type-specific

--- src/main/java/com/cloudbees/plugins/credentials/CredentialsUseListener.java
+++ src/main/java/com/cloudbees/plugins/credentials/CredentialsUseListener.java
@@ -19,20 +19,50 @@ import java.util.List;
 public interface CredentialsUseListener extends ExtensionPoint {
 
     /**
-     * Called when {@link Credentials} is read by an object.
+     * Called when {@link Credentials} is read by a run.
      *
      * @param c   The used Credentials.
-     * @param obj The object using the credentials.
+     * @param run The run using the credentials.
      */
-    void onUse(Credentials c, ModelObject obj);
+    void onUse(Credentials c, Run run);
 
     /**
-     * Fires the {@link #onUse} event to track the object that uses credentials.
-     *  @see CredentialsProvider#trackAll(Item, List)
+     * Called when {@link Credentials} is read by a node.
+     *
+     * @param c   The used Credentials.
+     * @param node The node using the credentials.
+     */
+    void onUse(Credentials c, Node node);
+
+    /**
+     * Called when {@link Credentials} is read by an item.
+     *
+     * @param c   The used Credentials.
+     * @param item The item using the credentials.
+     */
+    void onUse(Credentials c, Item item);
+
+    /**
+     * Fires the {@link #onUse} event to track the run that uses credentials.
      *  @see CredentialsProvider#trackAll(Run, List)
+     */
+    static void fireUse(Credentials c, Run run) {
+        Listeners.notify(CredentialsUseListener.class, true, listener -> listener.onUse(c, run));
+    }
+
+    /**
+     * Fires the {@link #onUse} event to track the node that uses credentials.
      *  @see CredentialsProvider#trackAll(Node, List)
      */
-    static void fireUse(Credentials c, ModelObject obj) {
-        Listeners.notify(CredentialsUseListener.class, true, listener -> listener.onUse(c, obj));
+    static void fireUse(Credentials c, Node node) {
+        Listeners.notify(CredentialsUseListener.class, true, listener -> listener.onUse(c, node));
+    }
+
+    /**
+     * Fires the {@link #onUse} event to track the item that uses credentials.
+     *  @see CredentialsProvider#trackAll(Item, List)
+     */
+    static void fireUse(Credentials c, Item item) {
+        Listeners.notify(CredentialsUseListener.class, true, listener -> listener.onUse(c, item));
     }
 }
