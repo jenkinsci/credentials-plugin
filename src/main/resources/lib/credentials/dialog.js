feeefe0d89139ce68dd27a274051d28129cd98f1
Dmitry_Platonov <platonovd32@gmail.com>
2022-10-12 11:10:18 +0300

    [JENKINS-69653] Un-inlining dialog.jelly

---
+++ src/main/resources/lib/credentials/dialog.js
@@ -0,0 +1,7 @@
+Behaviour.specify("#credentials-add-submit", 'credentials-dialog-add', 0, function (e) {
+    e.onclick = (_) => window.credentials.addSubmit(e);
+});
+
+Behaviour.specify("#credentials-add-abort", 'credentials-dialog-abort', 0, function (e) {
+    e.onclick = (_) => window.credentials.dialog.hide();
+});
