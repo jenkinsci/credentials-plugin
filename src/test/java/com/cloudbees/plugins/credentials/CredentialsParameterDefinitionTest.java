97b0fb62bdf03546745d6f175b654ebfc0eadd76
Tim Jacomb <timjacomb1@gmail.com>
2023-06-05 12:59:25 +0100

    Upgrade HtmlUnit from 2.x to 3.x

--- src/test/java/com/cloudbees/plugins/credentials/CredentialsParameterDefinitionTest.java
+++ src/test/java/com/cloudbees/plugins/credentials/CredentialsParameterDefinitionTest.java
@@ -27,7 +27,7 @@ package com.cloudbees.plugins.credentials;
 
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
-import com.gargoylesoftware.htmlunit.html.*;
+import org.htmlunit.html.*;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Launcher;
 import hudson.markup.MarkupFormatter;
@@ -124,11 +124,11 @@ public class CredentialsParameterDefinitionTest {
         assertFalse("List user credentials checkbox should not be checked by default", checkbox.isChecked());
         HtmlElement div = form.getOneHtmlElementByAttribute("div", "class", "warning user-credentials-caution");
         assertFalse("Caution message about user credentials should not be displayed yet", div.isDisplayed());
-        form.getSelectByName("_.value").getOptions().forEach(option -> assertNotEquals("No user credential should be an option yet", userCredentialId, option.getValueAttribute()));
+        form.getSelectByName("_.value").getOptions().forEach(option -> assertNotEquals("No user credential should be an option yet", userCredentialId, option.getValue()));
 
         HtmlElementUtil.click(checkbox);
         assertTrue("Caution message about user credentials should be displayed after checking the box", div.isDisplayed());
-        form.getSelectByName("_.value").getOptions().stream().filter(option -> option.getValueAttribute().equals(userCredentialId)).findAny()
+        form.getSelectByName("_.value").getOptions().stream().filter(option -> option.getValue().equals(userCredentialId)).findAny()
                 .orElseThrow(() -> new AssertionError("No credential found matching user credential id " + userCredentialId));
     }
 
