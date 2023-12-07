7f098a55ae9fa0b42472c77aa4573858836a5a50
Mark Waite <mark.earl.waite@gmail.com>
2023-12-07 11:22:57 -0700

    [JENKINS-72364] Close provider dialog window when cancel is clicked
    One change from https://github.com/jenkinsci/credentials-plugin/pull/491
    was applied to a reference that was not using Prototype.js.
    
    https://issues.jenkins.io/browse/JENKINS-72364 is the issue report.
    
    Confirmed interactively that I can see the problem before the change
    and that with this change the problem is resolved.

--- src/main/resources/lib/credentials/dialog.js
+++ src/main/resources/lib/credentials/dialog.js
@@ -3,5 +3,5 @@ Behaviour.specify("#credentials-add-submit", 'credentials-dialog-add', 0, functi
 });
 
 Behaviour.specify("#credentials-add-abort", 'credentials-dialog-abort', 0, function (e) {
-    e.onclick = (_) => window.credentials.dialog.style.display = "none";
+    e.onclick = (_) => window.credentials.dialog.hide();
 });
