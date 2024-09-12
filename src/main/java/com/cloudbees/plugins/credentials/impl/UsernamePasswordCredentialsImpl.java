d7bc2d143cd0010040fe2739660fa339c0010100
Olivier Lamy <olamy@apache.org>
2024-09-13 09:10:14 +1000

    do not use commons lang3
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/com/cloudbees/plugins/credentials/impl/UsernamePasswordCredentialsImpl.java
+++ src/main/java/com/cloudbees/plugins/credentials/impl/UsernamePasswordCredentialsImpl.java
@@ -34,7 +34,7 @@ import hudson.util.FormValidation;
 import hudson.util.Secret;
 
 import jenkins.security.FIPS140;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
