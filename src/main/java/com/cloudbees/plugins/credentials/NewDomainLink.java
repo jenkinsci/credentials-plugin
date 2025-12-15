ee4d6a369eb62eccbe8bb39838f25c50b19d5c36
Tim Jacomb <timjacomb1@gmail.com>
2025-12-15 22:34:58 +0000

    More progress

--- src/main/java/com/cloudbees/plugins/credentials/NewDomainLink.java
+++
@@ -1,7 +0,0 @@
-package com.cloudbees.plugins.credentials;
-
-public class NewDomainLink {
-    public String getDisplayName() {
-        return "New domain";
-    }
-}
---
+++ src/main/java/com/cloudbees/plugins/credentials/links/UpdateDomainLink.java
@@ -0,0 +1,7 @@
+package com.cloudbees.plugins.credentials.links;
+
+public class UpdateDomainLink {
+    public String getDisplayName() {
+        return Messages.UpdateDomainLink_displayName();
+    }
+}
