4e0655f5cb793a97c5efdf3153ea8b45dfa9430e
Tim Jacomb <timjacomb1@gmail.com>
2025-12-16 16:30:08 +0000

    Should be all the system pages

---
+++ src/main/java/com/cloudbees/plugins/credentials/links/DeleteCredentialsLink.java
@@ -0,0 +1,7 @@
+package com.cloudbees.plugins.credentials.links;
+
+public class DeleteCredentialsLink {
+    public String getDisplayName() {
+        return Messages.DeleteCredentialsLink_displayName();
+    }
+}
